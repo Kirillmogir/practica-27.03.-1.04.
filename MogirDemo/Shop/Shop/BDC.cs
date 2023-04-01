24085818d67dd24643dabfae7ddf5ed1a29ef525
Mogir Kiril <kirillmogir@yandex.ru>
2023-04-01 13:35:28 +0300

    Закончил практику

---
+++ MogirDemo/Shop/Shop/BDC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class BDC
+    {
+        private static EBD _db;
+        public static EBD db
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new EBD();
+                return _db;
+            }
+        }
+    }
+}
---
+++ MogirDemo/Shop/Shop/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var user = BDC.db.Пользователи.FirstOrDefault(q => q.Логин.Equals(login.Text) && q.Пароль.Equals(password.Password));
+                if (user != null)
+                {
+                    Window newwindow = new PageProducts();
+                    this.Close();       
+                    newwindow.Show();
+                }
+                else
+                    MessageBox.Show("Неверно введен логин или пароль");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
